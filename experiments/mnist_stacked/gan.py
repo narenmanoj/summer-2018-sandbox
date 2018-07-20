9f777ce5bb82fcb9d2b34d5ecd8459286bc3bcb0
Naren Manoj <manoj.narens@gmail.com>
2018-07-20 16:22:37 -0500

    changing to dcgan

--- experiments/mnist_stacked/gan.py
+++ experiments/mnist_stacked/gan.py
@@ -22,55 +22,80 @@ print("Cuda: " + str(cuda))
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 
+def dc_weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
 # defining generator and discriminator architecture, taken from Github Pytorch-GAN (need to modify hyperparams)
-class Generator(nn.Module):
-    def __init__(self,
-                 latent_dim=100,
-                 img_shape=(2, ),
-                 layer_width=400,
-                 grid_length=5):
+class DCGenerator(nn.Module):
+    def __init__(self, latent_dim=100, img_size=32, channels=3):
         super(Generator, self).__init__()
+        
+        self.latent_dim = 100
+        self.img_size = 32
+        self.channels = 3
 
-        self.img_shape = img_shape
-        self.grid_length = grid_length
+        self.init_size = self.img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(self.latent_dim, 128*self.init_size**2))
 
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.0, inplace=True))
-            return layers
-
-        self.model = nn.Sequential(
-            *block(latent_dim, layer_width, normalize=True),
-            *block(layer_width, layer_width),
-            *block(layer_width, layer_width),
-            *block(layer_width, layer_width),
-            nn.Linear(layer_width, int(np.prod(img_shape))),
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, self.channels, 3, stride=1, padding=1),
+            nn.Tanh()
         )
 
     def forward(self, z):
-        img = self.model(z)
-        img = img.view(img.size(0), *self.img_shape)
-        return torch.clamp(
-            img, min=-self.grid_length + 0.001, max=self.grid_length - 0.001)
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        return img
 
-
-class Discriminator(nn.Module):
-    def __init__(self, img_shape=(2, )):
+class DCDiscriminator(nn.Module):
+    def __init__(self, img_size=32, channels=3):
         super(Discriminator, self).__init__()
+        
+        self.img_size = 32
+        self.channels = 3
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [   nn.Conv2d(in_filters, out_filters, 3, 2, 1),
+                        nn.LeakyReLU(0.2, inplace=True),
+                        nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
 
         self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 200),
-            nn.LeakyReLU(0.2, inplace=True), nn.Linear(200, 200),
-            nn.LeakyReLU(0.2, inplace=True), nn.Linear(200, 1), nn.Sigmoid())
+            *discriminator_block(self.channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+        )
+
+        # The height and width of downsampled image
+        ds_size = self.img_size // 2**4
+        self.adv_layer = nn.Sequential( nn.Linear(128*ds_size**2, 1),
+                                        nn.Sigmoid())
 
     def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        validity = self.model(img_flat)
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
 
         return validity
-
 
 def sample_from_stacked_mnist():
     pass
