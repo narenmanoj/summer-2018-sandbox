bac058bdecc6e070af95c15b661448cede4466f1
Naren Manoj <manoj.narens@gmail.com>
2018-07-25 15:47:44 -0500

    cleaned up mnist classifier py

--- experiments/mnist_stacked/mnist_classifier.py
+++ experiments/mnist_stacked/mnist_classifier.py
@@ -24,10 +24,11 @@ cuda = torch.cuda.is_available()
 print("Cuda: " + str(cuda))
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-class Net(nn.Module):    
+
+class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-          
+
         self.features = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(32),
@@ -42,22 +43,21 @@ class Net(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-          
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
         self.classifier = nn.Sequential(
-            nn.Dropout(p = 0.5),
+            nn.Dropout(p=0.5),
             nn.Linear(64 * 8 * 8, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
-            nn.Dropout(p = 0.5),
+            nn.Dropout(p=0.5),
             nn.Linear(512, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
-            nn.Dropout(p = 0.5),
+            nn.Dropout(p=0.5),
             nn.Linear(512, 10),
         )
-          
+
         for m in self.features.children():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -65,26 +65,26 @@ class Net(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
-        
+
         for m in self.classifier.children():
             if isinstance(m, nn.Linear):
                 nn.init.xavier_uniform(m.weight)
             elif isinstance(m, nn.BatchNorm1d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
-                
 
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
-        
-        return x 
-    
+
+        return x
+
+
 def train(save_model=False, filename="", num_epochs=50, disp_interval=5):
     if save_model:
         assert len(filename) > 0
-        
+
     model = Net()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.003, amsgrad=True)
@@ -96,9 +96,9 @@ def train(save_model=False, filename="", num_epochs=50, disp_interval=5):
     if cuda:
         model = model.cuda()
         criterion = criterion.cuda()
-        
+
     dataloader = gan.get_mnist_dataloader(300)
-    
+
     for epoch in range(num_epochs):
         for batch_idx, (data, target) in enumerate(dataloader):
             data, target = Variable(data), Variable(target)
@@ -113,14 +113,15 @@ def train(save_model=False, filename="", num_epochs=50, disp_interval=5):
 
             loss.backward()
             optimizer.step()
-            
+
         if epoch % disp_interval == 0:
             print("[Epoch %d] [Train Loss: %f]" % (epoch, loss.item()))
-    
+
     if save_model:
         torch.save(model.state_dict(), filename)
-        
+
     return model
+
 
 def load_model(filename):
     model = Net()
@@ -129,27 +130,27 @@ def load_model(filename):
         model.cuda()
     return model
 
+
 def evaluate(model, data_loader):
     model.eval()
     loss = 0
     correct = 0
-    
+
     for data, target in data_loader:
         data, target = Variable(data, volatile=True), Variable(target)
         if torch.cuda.is_available():
             data = data.cuda()
             target = target.cuda()
-        
+
         output = model(data)
-        
+
         loss += F.cross_entropy(output, target, size_average=False).data[0]
 
         pred = output.data.max(1, keepdim=True)[1]
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-        
+
     loss /= len(data_loader.dataset)
-        
+
     print('\nAverage loss: {:.4f}, Accuracy: {}/{} ({:.3f}%)\n'.format(
         loss, correct, len(data_loader.dataset),
         100. * correct / len(data_loader.dataset)))
-        
